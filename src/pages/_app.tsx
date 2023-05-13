d787ed5f754209710eecf4bb69ae78b27dfd8d08
U631768 <fnquintana@teco.com.ar>
2023-05-13 11:09:33 -0300

    feat: wakeLock

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,26 @@
-import "@/styles/globals.css";
+import { useEffect } from "react";
 import type { AppProps } from "next/app";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      let screenLock: any;
+      document.addEventListener("visibilitychange", async () => {
+        if (screenLock !== null && document.visibilityState === "visible") {
+          screenLock = await window.navigator.wakeLock.request("screen");
+        }
+      });
+
+      return () => {
+        if (typeof screenLock !== "undefined" && screenLock != null) {
+          screenLock.release().then(() => {
+            screenLock = null;
+          });
+        }
+      };
+    }
+  }, []);
   return (
     <div className="p-4 h-screen max-h-screen">
       <Component {...pageProps} />
